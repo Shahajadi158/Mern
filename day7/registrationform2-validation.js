482a61cb076b90a6b0b1c9e8a2b925bf610c8ea5
Shahajadi158 <skshahajadi1@gmail.com>
2024-07-27 02:31:35 -0700

    Add files via upload

---
+++ day7/registrationform2-validation.js
@@ -0,0 +1,9 @@
+function formValidation()
+{
+  /*var name=document.forms.['registration']['uname'];
+  or
+  var name=document.getElementById('uname').value;
+    or*/
+
+var uid = document.registration.userid;
+var passid = document.registration.passid;
